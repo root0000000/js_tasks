38ab9b48f56d914e92ff6098d899b7a9d6c7b226
root0000000 <arsen.siversky@gmail.com>
2025-11-14 12:55:30 +0100

    Refactor MyFunction and event listener syntax

--- 5.js
+++ 5.js
@@ -1,9 +1,9 @@
-      function MyFunction() {
-        var input1 = document.getElementById("input1").value;
-        if (input1.length >= 6 && input1.endsWith("kot")) {
-          result.innerText = "Word does have 6 characters and ends with kot";
-        } else {
-          result.innerText = "Word doesn't have 6 characters neither ends with kot";
-        }
-      }
-      button.addEventListener("click", () => MyFunction());
+function MyFunction() {
+  var input1 = document.getElementById("input1").value;
+  if (input1.length >= 6 && input1.endsWith("kot")) {
+    result.innerText = "Word does have 6 characters and ends with kot";
+  } else {
+    result.innerText = "Word doesn't have 6 characters neither ends with kot";
+  }
+}
+button.addEventListener('click', MyFunction);
