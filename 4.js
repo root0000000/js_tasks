b3e825c160df1964c62cf30bb2fecccb6f930403
root0000000 <arsen.siversky@gmail.com>
2025-11-14 11:16:46 +0100

    Refactor MyFunction to parse input values as numbers

--- 4.js
+++ 4.js
@@ -1,10 +1,13 @@
-      function MyFunction() {
-        var input1 = document.getElementById("input1").value;
-        var input2 = document.getElementById("input2").value;
-        var input3 = document.getElementById("input3").value;
-        var small = Math.min(input1, input2, input3);
-        var big = Math.max(input1, input2, input3);
-        alert("smallest" + small);
-        alert("biggest" + big);
-      }
-      button.addEventListener("click", () => MyFunction());
+function MyFunction() {
+  var input1 = document.getElementById("input1").value;
+  var input2 = document.getElementById("input2").value;
+  var input3 = document.getElementById("input3").value;
+  let a = parseFloat(input1);
+  let b = parseFloat(input2);
+  let c = parseFloat(input3);
+  var small = Math.min(a, b, c);
+  var big = Math.max(a, b, c);
+  alert("smallest" + small);
+  alert("biggest" + big);
+}
+button.addEventListener("click", () => MyFunction());
